d4d1787f6e4d7e4d9e0149a5408ea4494e1c1d1d
domee1337 <a2@fs24.local>
2017-07-17 08:15:38 +0200

    upd

---
+++ src/Providers/FreePlacesServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+ 
+namespace FreePlaces\Providers;
+ 
+use Plenty\Plugin\ServiceProvider;
+ 
+class FreePlacesServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+ 
+    }
+}
+
+?>
